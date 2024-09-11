f96d8b30c471c5d65d8459b093f5d690103fb16c
GiovannaT <giovanna.trigueiro@gmail.com>
2024-09-10 23:30:19 -0300

    add libs

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react'
 
 interface Props{
     title: string;
 }
 
+
 export const Botao = ({title}: Props) => {
+
+  const getBooks = useCallback(async ()=>{
+    const response = await axios('https://potterapi-fedeperin.vercel.app/pt/books');
+    console.log(response.data);
+
+  },[])
+
+  useEffect(()=>{
+    console.log('teste');
+    getBooks();
+  }, [])
+
   return (
     <button type='button'>{title}</button>
   )
