1830ea4148b809156c69eb48a3ad10b17160d55d
GiovannaT <giovanna.trigueiro@gmail.com>
2024-10-01 20:43:55 -0300

    fix principal page

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
 import AddBook from './pages/addbook';
 import Login from './feature/login/Login';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/login",
     element: <Login />,
   },
   {
